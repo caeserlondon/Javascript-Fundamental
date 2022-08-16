7d2f43f3f3f659b1ed4bd05a3cc6d0053d9ced98
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-08-16 17:16:44 +0100

    Updating challange 6

--- index.js
+++ index.js
@@ -94,3 +94,31 @@ function reverseInt(int) {
 const output = reverseInt(-17893)
 
 //////////////////////////////////////////////////
+
+// CHALLENGE 6 :  LONGEST WORD
+// RETURN THE LONGEST WORD OF A STRING
+// if more than one the put into an Array
+
+function longestWord(sen) {
+	// create a filttered array
+	const wordArr = sen.toLowerCase().match(/[a-z0-9]+/g)
+
+	// sort by length
+	const sorted = wordArr.sort((a, b) => b.length - a.length)
+
+	// if multiple word put into array
+	const longestWordArr = sorted.filter(
+		(word) => word.length === sorted[0].length
+	)
+
+	// check if more than one word in the Array
+	if (longestWordArr.length === 1) {
+		// return the word
+		return longestWordArr[0]
+	} else {
+		return longestWordArr
+	}
+}
+const output = longestWord('Hello there, my name is Caeser')
+
+console.log(output)
