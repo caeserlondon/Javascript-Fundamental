8ebec8034d7530bd6c3b3a3f45d30e19edf79599
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-08-28 09:07:30 +0100

    Challenge 5

--- index.js
+++ index.js
@@ -99,3 +99,29 @@
 // 	});
 // }
 ///////////////////////////////////////////////
+
+// challenge 5 : maxCharecter
+// return the charecter that is most Comment
+
+// function maxCharecter(str) {
+// 	const charMap = {};
+// 	let maxNum = 0;
+// 	let maxChar = "";
+
+// 	str.split("").forEach(function (char) {
+// 		if (charMap[char]) {
+// 			charMap[char]++;
+// 		} else {
+// 			charMap[char] = 1;
+// 		}
+// 	});
+// 	for (let char in charMap) {
+// 		if (charMap[char] > maxNum) {
+// 			maxNum = charMap[char];
+// 			maxChar = char;
+// 		}
+// 	}
+
+// 	return maxChar;
+// }
+/////////////////////////////
