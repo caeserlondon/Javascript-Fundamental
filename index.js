ed5b12b38e16d8a2cc272b5e16ede2f4a845ce56
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-08-30 20:45:11 +0100

    Challenge 6

--- index.js
+++ index.js
@@ -125,3 +125,22 @@
 // 	return maxChar;
 // }
 /////////////////////////////
+/////////////////////////////
+// challenge 6 fizzBuzz
+
+// function fizzBuzz(num) {
+// 	for (let i = 1; i <= num; i++) {
+// 		if (i % 15 === 0) {
+// 			console.log("FizzBuzz");
+// 		} else if (i % 3 === 0) {
+// 			console.log("Fizz");
+// 		} else if (i % 5 === 0) {
+// 			console.log("Buzz");
+// 		} else {
+// 			console.log(i);
+// 		}
+// 	}
+// }
+
+// const output = fizzBuzz(100);
+console.log(output)
