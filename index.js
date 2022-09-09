9c5ad54882f76920be353162aeaf023c6c29e77c
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-09-09 22:16:21 +0100

    Challenge 12

--- index.js
+++ index.js
@@ -314,3 +314,30 @@
 // console.log(output);
 
 /////////////////////////////////////////////////
+
+//// CHALLENGE 12
+///// LONGEST WORDS IN THE STRING
+//// if there are more than one word return them in an array.
+
+function longestWords(str) {
+	let words = str.split(' ');
+	let size = 0;
+	let max = [''];
+
+	for (let i = 0; i < words.length; i++) {
+		if (words[i].length >= size) {
+			size = words[i].length;
+			if (max[max.length - 1].length < words[i].length) {
+				max = [];
+				max.push(words[i]);
+			} else {
+				max = [...max, words[i]];
+			}
+		}
+	}
+
+	return [...max];
+}
+
+console.log(longestWords('I woke up early today'));
+console.log(longestWords('I love javascript'));
