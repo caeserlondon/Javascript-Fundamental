12e040cbd1e42ef78a7c4a854403a06472cd8c30
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-09-17 12:49:44 +0100

    Challenge 17

--- index.js
+++ index.js
@@ -514,3 +514,18 @@
 // console.log(missingLetters("abce"));
 
 /////////////////////////////////////////////////////////
+/// CHALLENG 17 : EVEN AND ODD SUMS
+// take in an array and return an array of the sums of even and odd numbers
+/// ex  evenOddSums([50, 60, 60, 45, 71]) == [170, 116]
+// function evenOddSums(arr) {
+// 	let evenSum = 0;
+// 	let oddSum = 0;
+
+// 	arr.forEach((num) => (num % 2 === 0 ? (evenSum += num) : (oddSum += num)));
+
+// 	return [evenSum, oddSum];
+// }
+
+// console.log(evenOddSums([50, 60, 60, 45, 71]));
+
+////////////////////////////////////////////////
