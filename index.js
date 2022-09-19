c7f857837a7c0e0155cac6cc48fb5803fb6e120b
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-09-19 19:48:24 +0100

    Challenge 19

--- index.js
+++ index.js
@@ -546,3 +546,15 @@
 // console.log(longestWord("my name is caeser"));
 
 ///////////////////////////////////////////
+/// CHALLENGE 19 CAPITALIZE THE FIRST LETTER
+/// solution 1
+// function capitaize(str) {
+// 	let newArr = str.toLowerCase().split(" ");
+// 	for (let i = 0; i < newArr.length; i++) {
+// 		newArr[i] =
+// 			newArr[i].substring(0, 1).toUpperCase() + newArr[i].substring(1);
+// 	}
+// 	return newArr.join(" ");
+// }
+
+// console.log(capitaize("caeser al ani"));
