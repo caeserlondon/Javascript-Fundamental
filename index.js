7f7b6aed09da6241fb497ec706402270337536c4
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-09-22 08:01:23 +0100

    Challenge 21

--- index.js
+++ index.js
@@ -603,3 +603,35 @@
 // console.log(maxChar("javascripttt"));
 
 ///////////////////////////
+///////////////////////////
+/// Challenge 21
+/// find the math sequence..   (no negetive numbers)
+/// artimatic, geometric, or no pattern
+
+// function mathSequences(arr) {
+// 	let arith = new Set();
+// 	let geo = new Set();
+
+// 	for (let i = 1; i < arr.length; i++) {
+// 		let number1 = arr[i] - arr[i - 1];
+// 		arith.add(number1);
+// 		let number2 = arr[i] / arr[i - 1];
+// 		geo.add(number2);
+// 	}
+// 	if (arith.size === 1) {
+// 		return "Arithmatic";
+// 	}
+// 	if (geo.size === 1) {
+// 		return "Geometric";
+// 	}
+// 	return -1;
+// }
+
+// console.log(mathSequences([2, 4, 6, 8]));
+// /// 'Arithmatic'
+// console.log(mathSequences([3, 9, 27]));
+// /// 'Geometric'
+// console.log(mathSequences([2, 7, 16, 98]));
+// /// -1
+
+////////////////////////////////////////////
