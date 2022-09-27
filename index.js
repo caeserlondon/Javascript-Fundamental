26974e7513769b798346a1b76781e6bf61803a41
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-09-27 19:27:35 +0100

    Challenge 24

--- index.js
+++ index.js
@@ -717,3 +717,21 @@
 // //false
 
 /////////////
+// ///solution 2 using an object
+
+// function unique(str) {
+// 	let values = {};
+// 	for (let letter of str) {
+// 		if (values[letter]) {
+// 			return false;
+// 		}
+// 		values[letter] = "exists";
+// 	}
+// 	return true;
+// }
+
+// console.log(unique("abcde"));
+// //true
+// console.log(unique("abacdefb"));
+// //false
+/////////////
