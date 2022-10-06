4316e0e23fff27837e817794b1bbfa8d56eb00ee
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-10-06 19:46:29 +0100

    Challenge 29

--- index.js
+++ index.js
@@ -882,3 +882,28 @@
 // console.log(countLetters("Javascript is the greatest programming languageee"));
 
 ///////////////////////////////////////////////////////
+// //////////// CHALLENGE 29
+// //// sort the array and find the second item from right and from left.
+// /// delete dublicate
+
+// function secondValue(arr) {
+// 	let values = [...new Set(arr)].sort((a, b) => a - b);
+// 	if (values.length < 2) {
+// 		return `${values[0]}`;
+// 	} else if (values.length === 2) {
+// 		return `${values[0]} ${values[1]}`;
+// 	} else {
+// 		return `${values[1]} ${values[values.length - 2]}`;
+// 	}
+// }
+
+// console.log(secondValue([1]));
+// //1
+// console.log(secondValue([4, 2]));
+// // 2 4
+// console.log(secondValue([11, 44, 22]));
+// //11,22,44 === 22 , 22
+// console.log(secondValue([3, 2, 88, 3, -11, 67, 7]));
+// // -11,2,3,7,67,88 === 2 , 67
+
+//////////////////////////////////////////////////////
