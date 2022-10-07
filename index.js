d37837b21fb8068719bf20c92a0c16f5b42a8fee
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-10-07 18:28:40 +0100

    Challenge 30

--- index.js
+++ index.js
@@ -907,3 +907,36 @@
 // // -11,2,3,7,67,88 === 2 , 67
 
 //////////////////////////////////////////////////////
+///////  CHALLENGE 30
+/// return number with most repeat
+/// if two numbers are repeated then repeat the first
+/// if none repeated then return -1
+
+// function testRepeat(arr) {
+// 	let max = 1;
+// 	let position = 0;
+// 	let value = -1;
+// 	let tempNumbers = arr.reduce((acc, curr, index) => {
+// 		acc[curr] = acc[curr]
+// 			? { ...acc[curr], amount: acc[curr]["amount"] + 1 }
+// 			: { amount: 1, index };
+// 		let amount = acc[curr].amount;
+// 		let place = acc[curr].index;
+// 		if (amount > max || (amount === max && place <= position && amount > 1)) {
+// 			max = amount;
+// 			value = curr;
+// 			position = place;
+// 		}
+// 		return acc;
+// 	}, {});
+// 	return value;
+// }
+
+// console.log(testRepeat([5, 2, 2, 1, 5]));
+// // return 5
+// console.log(testRepeat([6, 5, 5, 10, 10, 10]));
+// // return 10
+// console.log(testRepeat([3, 4, 1, 6, 10]));
+// // return -1;
+
+/////////////////////////////////////////////////////////////
