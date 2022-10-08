7e452b21355e01cee1d41fb0b65b553eeabc4286
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-10-08 18:45:27 +0100

    Challenge 31

--- index.js
+++ index.js
@@ -940,3 +940,27 @@
 // // return -1;
 
 /////////////////////////////////////////////////////////////
+///// CHALLENGE 31
+//prime number = can only be divided by itself or one.
+
+/// solution 1
+// function isPrime(num) {
+// 	if (num < 2) {
+// 		return false;
+// 	}
+// 	for (let i = 2; i < num; i++) {
+// 		if (num % i === 0) {
+// 			return false;
+// 		}
+// 	}
+// 	return true;
+// }
+
+// console.log(isPrime(8));
+// // false
+// console.log(isPrime(11));
+// // true
+// console.log(isPrime(121));
+// // false
+// console.log(isPrime(127));
+// // true
