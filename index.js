35dfe8bab3e114343b73b192ff559c8f0b1d4e5a
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-10-09 18:47:34 +0100

    Challenge 31

--- index.js
+++ index.js
@@ -964,3 +964,28 @@
 // // false
 // console.log(isPrime(127));
 // // true
+///////// solution 2 : faster for big numbers.
+
+// function isPrime(num) {
+// 	if (num < 2) {
+// 		return false;
+// 	}
+// 	let root = Math.ceil(Math.sqrt(num));
+
+// 	for (let i = 2; i <= root; i++) {
+// 		if (num % i === 0) {
+// 			return false;
+// 		}
+// 	}
+// 	return true;
+// }
+
+// console.log(isPrime(8));
+// // false
+// console.log(isPrime(11));
+// // true
+// console.log(isPrime(121));
+// // false
+// console.log(isPrime(127));
+// // true
+////////////////////////////////////////////////
