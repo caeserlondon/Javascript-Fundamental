cf2f5b4edb7a3ce6a7432067fff65a267bc79d25
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-10-10 20:41:57 +0100

    Challenge 32

--- index.js
+++ index.js
@@ -989,3 +989,27 @@
 // console.log(isPrime(127));
 // // true
 ////////////////////////////////////////////////
+//// CHALLENGE 32
+/// count repeating letters
+// /// string will not contain numbers or sumbols..  only letters
+
+// function countLetters(str) {
+// 	let tempArr = str.split("");
+// 	let letters = [];
+// 	let count = 1;
+
+// 	for (let i = 0; i < tempArr.length; i++) {
+// 		if (tempArr[i] === tempArr[i + 1]) {
+// 			count++;
+// 		} else {
+// 			let value = `${count}${tempArr[i]}`;
+// 			letters = [...letters, value];
+// 			count = 1;
+// 		}
+// 	}
+// 	return letters.join("");
+// }
+// console.log(countLetters("ffffeerttttooo"));
+// /// should return 4f2e1r4t3o
+
+//////////////////////////////////////////
