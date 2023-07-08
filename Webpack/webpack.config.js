2f1b0726182f6c8a6383520fcdc4a716f04e7ba0
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2023-07-08 19:02:42 +0100

    Setting up the loaders

--- Webpack/webpack.config.js
+++ Webpack/webpack.config.js
@@ -11,4 +11,12 @@ module.exports = {
 		path: path.resolve(__dirname, 'dist'),
 		filename: '[name].js',
 	},
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+		],
+	},
 };
