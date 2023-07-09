339b0ad35bd93717af2972a8f61b8a59cc370b8d
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2023-07-09 21:30:14 +0100

    Set and config Axios

--- Webpack/src/generatejoke.js
+++ Webpack/src/generatejoke.js
@@ -7,7 +7,9 @@ function generatejoke() {
 		},
 	};
 
-	axios.get('https://icanhazdadjoke.com', config);
+	axios.get('https://icanhazdadjoke.com', config).then((res) => {
+		document.getElementById('joke').innerHTML = res.data.joke;
+	});
 }
 
 export default generatejoke;
