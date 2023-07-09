d61a6d530a527e078a26038293a7141f333168c9
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2023-07-09 16:15:39 +0100

    Updating the web config

--- Webpack/webpack.config.js
+++ Webpack/webpack.config.js
@@ -11,12 +11,40 @@ module.exports = {
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: '[name][contenthash].js',
+		clean: true, //  remove the old output
+		assetModuleFilename: '[name][ext]', // to prevent renaming the assets
+	},
+	devtool: 'source-map',
+	devServer: {
+		static: {
+			directory: path.resolve(__dirname, 'dist'),
+		},
+		port: 3000, // default port is 3000
+		// open: true, //to open the browser automatically
+		open: false,
+		hot: true, // hot reloding is enabled
+		compress: true, // enable G-Zip Compression
+		historyApiFallback: true,
 	},
 	module: {
 		rules: [
 			{
 				test: /\.scss$/,
 				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env'],
+					},
+				},
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
 			},
 		],
 	},
