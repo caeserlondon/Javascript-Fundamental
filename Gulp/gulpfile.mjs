40a89e9d21ce4065027d683f27e9524d22c181be
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2023-07-10 18:51:41 +0100

    Adding Sass and Watch

--- Gulp/gulpfile.mjs
+++ Gulp/gulpfile.mjs
@@ -1,6 +1,8 @@
 import gulp from 'gulp';
+import concat from 'gulp-concat';
 import imagemin from 'gulp-imagemin';
 import uglify from 'gulp-uglify';
+import sass from 'sass';
 
 //* TOP LEVEL FUNCTIONS
 /*
@@ -11,12 +13,9 @@ gulp.watch  - Watch files and folder for changes
 */
 
 // Logs message
-gulp.task(`message`, function () {
-	return console.log('Gulp is running...');
-});
-
-gulp.task(`default`, function () {
-	return console.log('Gulp is running in default mode');
+gulp.task('message', function (done) {
+	console.log('Gulp is running...');
+	done();
 });
 
 // Copy all HTML files
@@ -29,18 +28,59 @@ gulp.task('copyHtml', function (done) {
 
 // Optimize Images
 // prettier-ignore
-gulp.task('imagemin', function (done) {		
+gulp.task('imageMin', function (done) {		
 	gulp.src('src/images/*')
-	.pipe(imagemin())
-		.pipe(gulp.dest('dist/images'))
+		.pipe(imagemin())
+		.pipe(gulp.dest('dist/images'));
 	done();
 })
 
 // Minify JS
 // prettier-ignore
 gulp.task('minify', function (done) {
-	gulp.src('src/js/*.js')
+	gulp
+		.src('src/js/*.js')
 		.pipe(uglify())
 		.pipe(gulp.dest('dist/js'))
 	done();
 })
+
+// compile SASS
+// prettier-ignore
+// gulp.task('sass', function (done) {
+// 	gulp
+// 		.src('src/sass/*.scss')
+// 		.pipe(sass().on('error', sass.logError))
+// 		.pipe(gulp.dest('dist/css'));
+// 	done();
+// });
+
+// Scripts
+// gulp.task('scripts', function (done) {
+// 	gulp
+// 		.src('src/js/*.js')
+// 		.pipe(concat('main.js'))
+// 		.pipe(uglify())
+// 		.pipe(gulp.dest('dist/js'));
+// 	done()
+// })
+function scripts() {
+	return gulp
+		.src('src/js/*.js')
+		.pipe(concat('main.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('dist/js'));
+}
+
+gulp.task('scripts', scripts);
+
+// Watch
+gulp.task('watch', function () {
+	gulp.watch('src/js/*.js', gulp.series('scripts'));
+	gulp.watch('src/images/*', gulp.series('imageMin'));
+	gulp.watch('src/*.html', gulp.series('copyHtml'));
+});
+
+gulp.task('all', gulp.parallel('message', 'copyHtml'));
+
+// gulp.task('all', ['message', 'copyHtml', 'imagmin', 'minify']);
